709e907862f3d767f32d86c36451ce44d40ce5a3
rubenvalepoli <146942428+rubenvalepoli@users.noreply.github.com>
2024-02-17 10:01:39 +0100

    estudiando

---
+++ PROGRAMACION/estudiando/src/main/java/com/fpmislata/estudiando/persistence/BookRepository.java
@@ -0,0 +1,4 @@
+package com.fpmislata.estudiando.persistence;
+
+public interface BookRepository {
+}
---
+++ PROGRAMACION/estudiando/src/main/java/com/fpmislata/estudiando/persistence/impl/BookRepositoryImpl.java
@@ -0,0 +1,4 @@
+package com.fpmislata.estudiando.persistence.impl;
+
+public class BookRepositoryImpl {
+}
