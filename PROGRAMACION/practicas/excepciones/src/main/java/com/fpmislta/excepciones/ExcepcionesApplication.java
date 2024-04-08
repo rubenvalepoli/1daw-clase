2cb552a3411e2438e614d2e8f6f43c7e3ecac7b5
Ruben Valero <rvpolicarpo@gmail.com>
2024-04-08 21:40:01 +0200

    Excepciones ejercicio 1 y 2

--- PROGRAMACION/practicas/excepciones/src/main/java/com/fpmislta/excepciones/ExcepcionesApplication.java
+++ PROGRAMACION/practicas/excepciones/src/main/java/com/fpmislta/excepciones/ExcepcionesApplication.java
@@ -3,11 +3,63 @@ package com.fpmislta.excepciones;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Scanner;
+
 @SpringBootApplication
 public class ExcepcionesApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ExcepcionesApplication.class, args);
+		//SpringApplication.run(ExcepcionesApplication.class, args);
+
+		Scanner reader = new Scanner(System.in);
+
+		int edad;
+
+		System.out.print("introduce la edad");
+		edad = reader.nextInt();
+
+		try {
+			if (verificarEdad(edad)){
+				System.out.println("puede pasar");
+			}else{
+				System.out.println("No puede pasar");
+
+			}
+		}catch (Exception e){
+			System.out.println("Algo salio mal");
+		}finally {
+			reader.close();
+		}
+
+
 	}
+	public static boolean verificarEdad(int edad) {
+		return edad > 18;
+	}
+
+
+
+
+
+
+
+
+
+/*	EJERCICIO 1A Y 1B
+
+	int multiplicado, dos;
+
+	dos = 2;
+
+		try {
+			System.out.print("Escribe un numero para multipliacarlo por 2: ");
+			multiplicado = reader.nextInt();
+			System.out.println("Resultado: " + (multiplicado * dos));
+		} catch (Exception e){
+			System.out.println("Algo ha salido mal");
+		}finally {
+			reader.close();
+		}
+	}*/
 
 }
