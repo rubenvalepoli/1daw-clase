c807464bbb13d13abb8a4ed280138b6102904e24
rubenvalepoli <146942428+rubenvalepoli@users.noreply.github.com>
2024-04-16 12:32:36 +0200

    Programacion ejercicios Excepciones

--- PROGRAMACION/practicas/excepciones/src/main/java/com/fpmislta/excepciones/ExcepcionesApplication.java
+++ PROGRAMACION/practicas/excepciones/src/main/java/com/fpmislta/excepciones/ExcepcionesApplication.java
@@ -8,36 +8,28 @@ import java.util.Scanner;
 @SpringBootApplication
 public class ExcepcionesApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args){
 		//SpringApplication.run(ExcepcionesApplication.class, args);
-
 		Scanner reader = new Scanner(System.in);
 
-		int edad;
+		/*EJERCICIO 2D*/
 
-		System.out.print("introduce la edad");
-		edad = reader.nextInt();
+		System.out.print("introduce la edad: ");
+		int edad = reader.nextInt();
+		verificarEdad(edad);
+		System.out.println("Puedes pasar");
 
-		try {
-			if (verificarEdad(edad)){
-				System.out.println("puede pasar");
-			}else{
-				System.out.println("No puede pasar");
-
-			}
-		}catch (Exception e){
-			System.out.println("Algo salio mal");
-		}finally {
-			reader.close();
+	}
+	public static boolean verificarEdad(int edad){
+		if (edad < 18){
+			throw new RuntimeException();
 		}
-
-
-	}
-	public static boolean verificarEdad(int edad) {
-		return edad > 18;
+		return true;
 	}
 
 
+
+	}
 
 
 
@@ -62,4 +54,88 @@ public class ExcepcionesApplication {
 		}
 	}*/
 
-}
+
+
+		/*EJERCICIO 2A
+
+
+		System.out.print("introduce la edad: ");
+		int edad = reader.nextInt();
+
+		try {
+			if (verificarEdad(edad)){
+				System.out.println("puede pasar");
+			}else{
+				System.out.println("No puede pasar");
+
+			}
+		}catch (Exception e){
+			System.out.println("Algo salio mal");
+		}finally {
+			reader.close();
+		}
+
+
+	}
+	public static boolean verificarEdad(int edad) {
+		if (edad < 18){
+			return false;
+		}
+		return true;
+	} */
+
+
+			/*EJERCICIO 2B
+
+			try {
+			System.out.print("introduce la edad: ");
+			int edad = reader.nextInt();
+			verificarEdad(edad);
+			System.out.println("Puedes pasar");
+
+			}catch (Exception e){
+				System.out.println("Tienes que ser mayor de edad para pasar");
+			}finally {
+				reader.close();
+			}
+
+
+		}
+		public static boolean verificarEdad(int edad) throws Exception {
+			if (edad < 18){
+				throw new Exception();
+			}
+			return true;
+		}*/
+
+		/*EJERCICIO 2C
+		//Muestra un error porque no hemos importado las excepciones y da error al acceder a la variable
+		//Lo podemos solucionar poniendo  throws Exception a continuacion de la funcion del main
+
+		System.out.print("introduce la edad: ");
+		int edad = reader.nextInt();
+		verificarEdad(edad);
+		System.out.println("Puedes pasar");
+
+	}
+	public static boolean verificarEdad(int edad) throws Exception {
+		if (edad < 18){
+			throw new Exception();
+		}
+		return true;
+	}*/
+
+		/*EJERCICIO 2D
+
+		System.out.print("introduce la edad: ");
+		int edad = reader.nextInt();
+		verificarEdad(edad);
+		System.out.println("Puedes pasar");
+
+	}
+	public static boolean verificarEdad(int edad){
+		if (edad < 18){
+			throw new RuntimeException();
+		}
+		return true;
+	}*/
