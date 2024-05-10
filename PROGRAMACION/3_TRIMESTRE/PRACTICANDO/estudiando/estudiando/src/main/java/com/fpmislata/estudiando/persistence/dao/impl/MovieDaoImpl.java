c2858a1383fed7a3a94b2164d5d55f4798a0fb67
rubenvalepoli <146942428+rubenvalepoli@users.noreply.github.com>
2024-05-10 13:45:45 +0200

    viernes

--- PROGRAMACION/3_TRIMESTRE/PRACTICANDO/estudiando/estudiando/src/main/java/com/fpmislata/estudiando/persistence/dao/impl/MovieDaoImpl.java
+++ PROGRAMACION/3_TRIMESTRE/PRACTICANDO/estudiando/estudiando/src/main/java/com/fpmislata/estudiando/persistence/dao/impl/MovieDaoImpl.java
@@ -4,6 +4,7 @@ import com.fpmislata.estudiando.persistence.bd.rawSql;
 import com.fpmislata.estudiando.persistence.dao.MovieDao;
 import com.fpmislata.estudiando.persistence.dao.entity.DirectorEntity;
 import com.fpmislata.estudiando.persistence.dao.entity.MovieEntity;
+import com.fpmislata.estudiando.persistence.dao.mapper.ActorEntityMapper;
 import com.fpmislata.estudiando.persistence.dao.mapper.MovieEntityMapper;
 
 import java.sql.ResultSet;
@@ -31,25 +32,33 @@ public class MovieDaoImpl implements MovieDao {
     }*/
 
     @Override
-    public List<MovieEntity> getAll(){
+    public List<MovieEntity> getAll() {
         List<MovieEntity> movieEntityList = new ArrayList<>();
-        ResultSet resultSet = rawSql.select("SELECT id , name, year, id_director FROM movie;",null);
+        ResultSet resultSet = rawSql.select("SELECT id , name, year, id_director FROM movie;", null);
         movieEntityList = (MovieEntityMapper.toMovieEntityList(resultSet));
         return movieEntityList;
     }
+
     @Override
     public MovieEntity findById(int id) throws SQLException {
 
         MovieEntity movieEntity;
-        List<Object> listMovieId= List.of(id);
+        List<Object> listMovieId = List.of(id);
         ResultSet resultSet = rawSql.select("""
-            SELECT m.* FROM movie m
-            INNER JOIN characterMovie cm
-            ON cm.movieId = m.id
-            and cm.id = ?;
-                """,null);
+                SELECT m.* FROM movie m
+                INNER JOIN characterMovie cm
+                ON cm.movieId = m.id
+                and cm.id = ?;
+                    """, null);
 
 
+        try {
+            resultSet.next();
+        }catch (SQLException e){
+            throw new RuntimeException(e);
+        }
+        return MovieEntityMapper.toMovieEntity(resultSet);
+    }
 
         /*for (MovieEntity movieEntity : movieEntityList){
             if (id == movieEntity.getId()){
@@ -57,7 +66,7 @@ public class MovieDaoImpl implements MovieDao {
             }
         }
         return null;*/
-    }
+    
 
 
 
